c6f9537eed186fd3b6f55948b28c05ea22b48e49
naatii <jcorber855@gmail.com>
2025-03-13 17:11:55 +0100

    Ultimo intento

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
-set -e  # Detiene el script si ocurre un error
-
-# Ejecuta Composer para instalar las dependencias de producción
-echo "Ejecutando Composer..."
+echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-# Cachea la configuración y las rutas
-echo "Cacheando configuración..."
+echo "Caching config..."
 php artisan config:cache
+
+echo "Caching routes..."
 php artisan route:cache
 
-# Ejecuta las migraciones si es necesario
-echo "Ejecutando migraciones..."
+echo "Running migrations..."
 php artisan migrate --force
-
-# Inicia PHP-FPM
-echo "Iniciando PHP-FPM..."
-php-fpm &
-
-# Inicia Nginx
-echo "Iniciando Nginx..."
-nginx -g "daemon off;"
